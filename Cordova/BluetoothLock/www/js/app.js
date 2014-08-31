1256e53beeec61d35d908a45dba41859a37da96c
Alex Albino <webmaster@alexventure.com>
2014-08-31 15:49:33 -0700

    Adding in BLE writeCharacteristic Example... Sends 1 byte to IMUduino

--- Cordova/BluetoothLock/www/js/app.js
+++ Cordova/BluetoothLock/www/js/app.js
@@ -48,141 +48,10 @@ THE SOFTWARE.
 // default to 'starter' as the module name. No problem, still works. :-)
 angular.module('starter', ['ionic']) // Requires the 'ionic' angular module.
 
-        // Let's define a controller. Since we only need one for this example,
-        // it's just called "Main". (Not very creative, but why not)
-        .controller(
-                'MainCtrl',
-                // Add in stuff we may need. Adding in $ionicPlatform since 
-                // I want to be sure it's available before doing stuff.
-                // (Maybe not necessary, but why not. Remove it and test if 
-                // you see fit)
-                ['$scope', '$ionicPlatform', '$timeout',
-                  function($scope, $ionicPlatform, $timeout) {
-
-                    // List of BLE devices found during scan.
-                    $scope.devices = {};
-                    $scope.isScanning = false; // Flag to manage ble scanning.
-                    $scope.msInterval = 1000; // Scan every 1000ms (1 second)
-                    $scope.msTimeout = 10; // Scan for 5 seconds, max..
-                    $scope.msTick = 0; // Keep track of how many intervals have passed
-                    
-                    $scope.tmrStartScan = null;
-                    
-                    $scope.debug = '';
-
-                    // This method is used to dynamically size the 'rssi' bar.
-                    $scope.getRssiWidth = function(device) {
-                      var rssiWidth = Math.max(0, 100 + (device.rssi));
-
-                      return rssiWidth;
-                    };
-
-                    // Internal startScan() method, handles cordova plugin usage.
-                    $scope.startScan = function(callbackFun) {
-
-                      // ...Stop the cordova plugin, just in case.
-                      $scope.stopScan();
-                      $scope.debug += 'startScan()';
-
-                      // ...Ok, tell the cordova plugin to start scanning!
-                      // Then, delegate to the supplied callback.
-                      window.evothings.ble.startScan(
-                              function(device) {
-                                callbackFun(device, null);
-                              },
-                              function(errorCode) {
-                                callbackFun(null, errorCode);
-                              }
-                      );
-                    };
-                    
-                    // Internal stopScan() method, handles cordova plugin usage.
-                    $scope.stopScan = function() {
-                      $scope.debug += 'stopScan()' + $scope.isScanning;
-                      // Tell the cordova plugin to stop scanning.
-                      window.evothings.ble.stopScan();
-                    };
-
-
-                    // UI Event handling: Start Scan!
-                    $scope.onStartScanButton = function() {
-                      $scope.display_status = 'Scanning...';
-
-                      // Already scanning? ignore, and return.
-                      
-                      // Set our flag, so we know we are scanning.
-                      $scope.isScanning = true;
-                      // Not scanning yet. Let's scan every so often.
-                      
-                      $scope.startScan($scope.deviceFound); // First off
-                      $scope.tmrStartScan = $timeout(function() { // Subsequent checks
-//                        if ($scope.msTick > $scope.msTimeout) {
-//                          $scope.isScanning = false;
-//                          $scope.msTick = 0;
-//                        }
-                        if (!$scope.isScanning) {
-                          $scope.debug += '[not scanning, cancel timeout]';
-                          // Check to see if we should still be scanning...
-                          // Nope! Return!
-                          $timeout.cancel($scope.tmrStartScan);
-                          return;
-                        }
-                        $scope.msTick++;
-                        $scope.debug += 'startScan() ' + $scope.isScanning + ' [' + $scope.msTick + ']';
-                        // Scan using our internall method!
-                        
-                        $scope.startScan($scope.deviceFound);
-                        
-                        // Do it again.
-                        $timeout.cancel($scope.tmrStartScan);
-                        
-                        if ($scope.isScanning && $scope.msTick < $scope.msTimeout) {
-                          $scope.onStartScanButton();
-                        } else {
-                          $scope.stopScan();
-                          $scope.isScanning = false;
-                          $scope.msTick = 0;
-                          
-                          return;
-                        }
-
-                      }, $scope.msInterval); // ... Timing interval
-
-
-
-                    };
-                    // UI Event handling: Stop scan!
-                    $scope.onStopScanButton = function() {
-                      $scope.isScanning = false;
-                      $timeout.cancel($scope.tmrStartScan);
-                      $scope.stopScan(); // Stop!!!!
-                      $scope.devices = {}; // Clear out the collection of devices.
-                      
-                      $scope.display_status = 'Scan Paused';
-                      $scope.debug = '';
-
-                    };
-
-                    // Callback for the cordova ble startScan() method.
-                    $scope.deviceFound = function(device, errorCode) {
-                      
-                      $scope.debug = "deviceFound()";
-                      if (device) {
-                        // We have a device to list! Add it to the collection!
-                        $scope.devices[device.address] = device;
-
-                      } else if (errorCode) {
-                        // Hmm... Something went wrong.
-                        $scope.display_status = 'Scan Error: ' + errorCode;
-                      }
-                    };
-
-
-                  }])
-
         // ...Stuff to get the ionic platform running.
         .run(function($ionicPlatform) {
           $ionicPlatform.ready(function() {
+            console.log('run() $ionicPlatform.ready()');
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -194,4 +63,319 @@ angular.module('starter', ['ionic']) // Requires the 'ionic' angular module.
             }
 
           });
-        });
+        })
+        .service('Spp', ['$ionicPlatform', '$q', function ($ionic, $q) {
+            
+            this.debug = []
+          
+          
+            this.ble;
+            this.scannedDevices = [];
+            this.isScanning = false;
+            this.activeDeviceHandle;
+            this.activeDeviceServices;
+            this.activeCharacteristicWrite;
+            this.activeCharacteristicRead;
+            
+            this.uuidUART_Service = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+            this.uuidUART_RX      = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+            this.uuidUART_TX      = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+
+            this.scope;
+            
+            var self = this;
+            
+            
+            self.initialize = function ($ionic, $scope) {
+              self.debug.push('initialize()');
+              
+              self.scope = $scope;
+              window.setTimeout(function () {
+                self.bindEvents();
+              }, 1000);
+              
+            };
+            
+            self.bindEvents = function () {
+              self.ble = window.evothings.ble;
+              self.debug.push('bindEvents()');
+              self.debug.push(typeof $ionic);
+              
+              if (window.hyper) {
+                window.hyper.onReload(function () {
+                  self.ble.stopScan();
+                  self.closeAll();
+                });
+              }
+              
+              // We were initialized by a controller, should be 
+              // safe to just call onDeviceReady()
+              
+              self.onDeviceReady();
+            };
+            
+            self.closeAll = function () {
+              self.debug.push('closeAll()');
+              
+              if (self.scannedDevices.length > 0) {
+                for (var i = 0; i < self.scannedDevices.length; i++) {
+                  self.ble.close( self.scannedDevices[i].deviceHandle );
+                }
+              }
+            };
+            
+            self.onDeviceReady = function () {
+              self.debug.push('onDeviceReady()');
+              console.log('onDeviceReady!');
+              
+              
+              self.isScanning = true;
+              
+              self.list();
+            };
+            
+            self.list = function () {
+              this.debug.push('list()');
+              console.log('list()');
+              // Show progress indicator
+              
+              // Scan with interval for 3 seconds
+//              window.setTimeout(function () {
+                self.ble.startScan(
+                  function(device) {
+                    console.log('startScan() returned device!');
+                    self.ble.stopScan();
+                    console.log('stopScan() called.')
+                    self.scannedDevices.push( device );
+                    
+                    console.log('Connecting to BLE device at ' + device.address + '...');
+                    self.connect( device.address );
+                  },
+                  function(errorCode) {
+                    console.log('startScan() returned error');
+                    console.log('Error! ' + errorCode);
+                  }
+                );
+//            }, 100);
+              
+            };
+            
+            self.connect = function (device_address) {
+              self.debug.push('connect()');
+              console.log('connect():' + device_address);
+              
+              self.ble.connect(
+                device_address,
+                function (connectInfo) {
+                  if (connectInfo.state == 2) { // Connected
+                    
+                    self.activeDeviceHandle = connectInfo.deviceHandle;
+                    console.log('Connected! Getting BLE services for device ' + connectInfo.deviceHandle);
+                    self.activeDeviceServices = self.getServices(connectInfo.deviceHandle);
+                  }
+                },
+                
+                function (errorCode) {
+                  self.debug.push('ble.connect() error');
+                  console.log('ble.connect() error: ' + errorCode);
+                }
+              );
+            };
+            
+            self.getServices = function (deviceHandle) {
+              self.debug.push('getServices()');
+              console.log('Reading services...');
+              window.setTimeout(function () {
+                
+                self.ble.services(deviceHandle, function (services) {
+                  // Find handles for characteristics and descriptor needed.
+
+                  console.log('device services count: ' + services.length);
+                  for (var si in services) {
+                     var service = services[si];
+
+
+                     console.log('getting characteristics for service ' + service.uuid + ' (' + service.handle + ')');
+                     
+                     self.ble.characteristics(
+                       deviceHandle,
+                       service.handle,
+                       
+                       function (characteristics) {
+                         
+                         for (var ch in characteristics) {
+                           var characteristic = characteristics[ch];
+                          // Read descriptors for characteristic
+                          console.log('Got characteristic for service ' + service.uuid + ': ' + JSON.stringify(characteristic));
+                          self.ble.descriptors(
+                            deviceHandle,
+                            characteristic.handle,
+
+                            function (descriptors) {
+                              
+                              if (descriptors && descriptors.length > 0) {
+                                console.log('BLE got descriptors for ' + service.uuid + '>' + characteristic.uuid  + '!' + JSON.stringify(descriptors));
+                              }
+
+                            },
+
+                            function (errorCode) {
+                              console.log('BLE get descriptors failed! error: ' + errorCode);
+                            }
+                          );
+
+                          if (characteristic.uuid == self.uuidUART_RX && !self.activeCharacteristicRead) {
+                            // try reading!
+                            
+                            console.log('GOT THE UART RX characteristic handle.');
+                            self.activeCharacteristicRead = characteristic.handle;
+                            
+                          }
+
+                          if (characteristic.uuid == self.uuidUART_TX && !self.activeCharacteristicWrite) {
+                            
+                            console.log('GOT THE UART TX characteristic handle.');
+                            self.activeCharacteristicWrite = characteristic.handle;
+                            
+                            // try writting!
+                            console.log('WRITING to ' + deviceHandle + ', ' + self.activeCharacteristicWrite);
+                            self.ble.writeCharacteristic(
+                              deviceHandle,
+                              self.activeCharacteristicWrite,
+                              new Uint8Array([1]),
+                              function () {
+                                console.log('* * * * * *WRITE SUCCESS!');
+                                
+                                
+                              },
+                                
+                              function (errorCode) {
+                                console.log('BLE writeCharacteristic to UART TX failed! error: ' + errorCode);
+                              }
+                            );
+                          
+                            
+                          }
+                        }
+                       },
+                       
+                       function (errorCode) {
+                         console.log('BLE get characteristics failed! error: ' + errorCode);
+                       }
+                     );
+                   
+                     
+                     /*
+                     for (var ci in service.characteristics) {
+                       var characteristic = service.characteristics[ci];
+                       console.log("Characteristic UUID: " + characteristic.uuid);
+                       // We already know the characteristic ids. Get their handles
+                       if (characteristic.uuid == self.uuidUART_TX && !self.activeCharacteristicWrite) {
+                         self.activeCharacteristicWrite = characteristic.handle;
+                       }
+
+                       if (characteristic.uuid == self.uuidUART_RX && !self.activeCharacteristicRead) {
+                         self.activeCharacteristicRead = characteristic.handle;
+                       }
+
+                       for (var di in characteristic.descriptors) {
+                         var descriptor = characteristic.descriptors[di];
+
+                         console.log('Found descriptor: ' + descriptor.uuid);
+                       }
+
+                     }
+                     */
+                  }
+
+                  if (self.activeCharacteristicRead && self.activeCharacteristicWrite) {
+                    console.log('***********Reading...');
+                    self.startReading( deviceHandle );
+                  }
+                });
+                
+              }, 500);
+            };
+            
+            self.startReading = function(deviceHandle) {
+              self.debug.push('startReading()');
+            };
+            
+            self.disconnect = function () {
+              
+              self.ble.stopScan();
+              self.closeAll();
+              
+//              self.debug = [];
+//              self.scannedDevices = [];
+//              self.activeCharacteristicRead = null;
+//              self.activeCharacteristicWrite = null;
+//              self.activeDeviceHandle = null;
+//              self.activeDeviceServices = null;
+            };
+            
+            self.onConnect = function () {
+              
+            };
+            
+            self.onDisconnect = function () {
+              
+            };
+            
+            self.onData = function () {
+              
+            };
+            
+            self.unlock = function () {
+              
+            };
+            
+            self.onDeviceList = function () {
+              
+            };
+            
+            self.showProgressIndicator = function () {
+              
+            };
+            
+            self.hideProgressIndicator = function () {
+              
+            };
+            
+            self.showUnlockScreen = function () {
+              
+            };
+            
+//            this.showDeviceListPage = function () {
+//              
+//            };
+            
+            this.setStatus = function () {
+              
+            };
+        }])
+      
+        // Let's define a controller. Since we only need one for this example,
+        // it's just called "Main". (Not very creative, but why not)
+        .controller(
+          'MainCtrl',
+          // Add in stuff we may need. Adding in $ionicPlatform since 
+          // I want to be sure it's available before doing stuff.
+          // (Maybe not necessary, but why not. Remove it and test if 
+          // you see fit)
+          ['$scope', '$ionicPlatform', 'Spp',
+            function($scope, ionic, Spp) {
+              console.log('MainCtrl()');
+              
+              
+              
+              $scope.debug = 'Main!';
+              $scope.Spp = Spp;
+              
+              ionic.ready(function () {
+                $scope.debug = 'ready!';
+                
+                $scope.Spp.initialize(ionic, $scope);
+              });
+              
+            }]);
